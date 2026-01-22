b7f456a6b491672868d2cf1099e320ec40423a73
toestedbwead <leielcano@gmail.com>
2026-01-22 18:14:13 +0800

    temp removing the role constraint so i could see what the problem is in accessing it

--- admin/login_process.php
+++ admin/login_process.php
@@ -22,8 +22,7 @@ $stmt->execute();
 $stmt->bind_result($id, $hashed_password, $role);
 $stmt->fetch();
 
-if ($stmt->num_rows === 1 && $role === 'superadmin' && password_verify($password, $hashed_password)) {
-    $_SESSION['logged_in'] = true;
+if ($stmt->num_rows === 1 && password_verify($password, $hashed_password)) {  // ← temporary, remove role check    $_SESSION['logged_in'] = true;
     $_SESSION['user_id']   = $id;
     $_SESSION['email']     = $email;
     $_SESSION['role']      = $role;
